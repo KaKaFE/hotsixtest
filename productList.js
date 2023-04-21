c1bf247ffc70d37f0fa0cd3556ef4cf66d56b552
KaKa <tnt@kakao.com>
2023-04-21 19:02:44 +0900

    Add files via upload

--- productList.js
+++ productList.js
@@ -1,80 +1,90 @@
-// 상품목록 HTML 추적
-const productRow = document.querySelector('.product-li');
+
+const productRow = document.querySelector('.product-li'); // 상품목록 HTML 추적
+let obj = null; // 상품목록 init
+let start = 1; // 상품목록 탐색을위한 초기값 상수
+const limit = 8; // 한페이지에 표시할 상품목록 갯수 상수
+
+
+fetchProducts();
+
+
+
 
 // 서버에서 상품목록 받아오기
-fetch('https://kakafe.github.io/hotsixtest/product.json')
+
+function fetchProducts() {
+    fetch('../product.json')
     .then(response => response.json())
     .then(data => {
-        console.log(data.products.length)
-        let remainProductArr = render(data);
-        return remainProductArr;
-    }).then(data => {
-        remainRender(data)
+        obj = data
+        render(start, limit)
     })
-
+}
 
 // img 경로 추후수정필요
-function render(obj) {
-    const renderIndex = 8; // 한페이지에 진열할 상품갯수 상수
-    const sliceLength = obj.products.length - renderIndex + 1; // 8개이후 남은 배열 인덱스값
-    const productsArr = obj.products
-    let productList = productRow.innerHTML.length ? productRow.innerHTML : '' // 초기innerhtml 이 비어있는지 아닌지 판단.
-    const products = obj.products.entries() //  배열 index 탐색을위한 선언
-    for (let [indx , i] of products) {
-        productList += 
-            `<div class="col-md-3">
-                <div class="card border-0">
-                <img src="./img/${i.thumnailImg}" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">${i.productName}
-                    <span class="badge rounded-pill ${i.concept}">${i.concept}</span>
-                    </h5>
-                    <p class="card-text d-flex justify-content-between align-items-end">₩ ${i.productPrice}
-                    <a class="nav-link" href="#"><i class="bi bi-cart3"></i></a>
-                    </p>
-                    </div>
-                </div>
-            </div>`;
-        if (indx + 1 === renderIndex && obj.products.length > renderIndex) {
-            productRow.innerHTML = moreBtnRender(productList) // 완성된 html에 더보기버튼추가
-            let remainproducts =
-                productsArr.slice(renderIndex - 1, renderIndex - 1 + sliceLength);
-            
-            remainproducts.length ? moreBtnRender(productList) : removeBtn();
 
-            return remainproducts;
-        }
+function render() {
+    const startIdx = (start - 1) * limit;
+    const endIdx = startIdx + limit;
+
+    for (let i = startIdx; i < Math.min(endIdx, obj.products.length) ; i++){
+        const product = obj.products
+        const productEl = creatProductEl(product[i])
+        productRow.appendChild(productEl);
     }
-    if (obj.products.length < renderIndex) {
-        productRow.innerHTML = productList;
-        removeBtn()
+    if (endIdx < obj.products.length) {
+        if(start !== 1)removeBtn();
+        moreBtnRender();
     }
 } 
 
-
-// 렌더링후 더보기버튼 추가 함수
-function moreBtnRender(innerHTML) {
-    innerHTML += 
-    `<div class="row more-btn-row">
-        <div class="col-12 text-center mt-5">
-            <button type='button' class="btn more-btn">더보기</button>
-        </div>
-    </div>`
-    return innerHTML;
+// 더보기 버튼 클릭시 제 랜더링 실행함수
+function onloadMore() {
+    start++;
+    render();
+    const endIdx = start * limit
+    if(endIdx >= obj.products.length) removeBtn()
 }
+
+// productHTML 생성 함수
+function creatProductEl(product) {
+    const productEl = document.createElement('div');
+    productEl.className = 'col-md-3';
+    productEl.innerHTML =`
+    <div class="card border-0">
+      <img src="../img/${product.thumnailImg}" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">${product.productName}
+          <span class="badge rounded-pill ${product.concept}">${product.concept}</span>
+        </h5>
+        <p class="card-text d-flex justify-content-between align-items-end">₩ ${product.productPrice}
+          <a class="nav-link" href="#"><i class="bi bi-cart3"></i></a>
+        </p>
+      </div>
+    </div>
+  `;
+    return productEl;
+}
+// 렌더링후 더보기버튼 추가 함수
+function moreBtnRender(start, limit) {
+    start, limit;
+    const morebtnEl = document.createElement('div');
+    morebtnEl.className = 'row more-btn-row';
+    const morebtnEl_div = document.createElement('div');
+    morebtnEl_div.className = 'col-12 text-center mt-5';
+    morebtnEl.appendChild(morebtnEl_div);
+    const morebtnEl_div_btn = document.createElement('button');
+    morebtnEl_div_btn.className = 'btn more-btn';
+    morebtnEl_div_btn.type = 'button';
+    morebtnEl_div_btn.textContent = '더보기'
+    morebtnEl_div_btn.addEventListener('click', onloadMore);
+    morebtnEl_div.appendChild(morebtnEl_div_btn);
+    productRow.appendChild(morebtnEl);
+}
+
 
 // 추가 물품 렌더링후 더보기버튼 제거 함수
 function removeBtn() {
     document.querySelector('.more-btn-row').remove()
 }
 
-function remainRender(arr) {
-    const morebtn = document.querySelector('.more-btn');
-    morebtn.addEventListener('click', (e) => {
-        e.preventDefault()
-        render({
-            "products" : arr
-        })
-        removeBtn()
-    })
-}
